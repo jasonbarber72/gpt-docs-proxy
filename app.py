118e1b4b5fccfcc6a6daba0b0085153b3a59aab3
jasonbarber72 <jasonbarber72@gmail.com>
2025-07-03 18:43:56 +1000

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,6 @@ def write_to_doc(doc_id):
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
