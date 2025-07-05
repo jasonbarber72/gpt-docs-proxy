2788fea08233a8ad3d6052e2cdd28aa433ce2bc2
jasonbarber72 <jasonbarber72@gmail.com>
2025-07-05 10:34:40 +1000

    Use correct port for Render deployment

--- app.py
+++ app.py
@@ -145,4 +145,6 @@ def write_to_doc(doc_id):
 
 
 if __name__ == "__main__":
-    app.run(port=8765)
+    port = int(os.environ.get("PORT", 8765))  # Use 8765 locally, Render's port in deployment
+    app.run(host="0.0.0.0", port=port)
+
